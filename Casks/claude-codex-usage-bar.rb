829209ee570b4698ebbe958e88205e1c266fb6ee
shutoinagaki <shutoinagaki01@agoop.co.jp>
2026-07-31 16:44:16 +0900

    release: 0.3.3

--- Casks/claude-codex-usage-bar.rb
+++ Casks/claude-codex-usage-bar.rb
@@ -1,6 +1,6 @@
 cask "claude-codex-usage-bar" do
-  version "0.3.2"
-  sha256 "06c2c48dc859080fa38a00f48766a639e37f245afb506b26ed1641b277cb0f8b"
+  version "0.3.3"
+  sha256 "c04e9f80d93ab36a6632f1d5082fffeb181282b317edb395a209cb1eb257bc1a"
 
   url "https://github.com/shutoinagaki01-agoop/ClaudeCodexUsageBar/releases/download/v#{version}/ClaudeCodexUsageBar.zip"
   name "ClaudeCodexUsageBar"
